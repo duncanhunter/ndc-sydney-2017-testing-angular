b812a6843dc010485509a4fd096e0f940e029191
Duncan Hunter <duncan@duncanhunter.com.au>
2017-08-16 08:55:15 +1000

    update e2e

--- e2e/joke.e2e-spec.ts
+++ e2e/joke.e2e-spec.ts
@@ -1,23 +1,46 @@
-import { JokePage } from './joke.po';
+import { element, by, browser } from 'protractor';
 
-describe(`Page: Joke Page`, () => {
-  it(`should have a title of "Chuck Norris Jokes"`, async () => {
-    JokePage.navigateTo();
+describe(`JOKE PAGE`, () => {
+  it(`should have a title of 'Chuck Norris Jokes'`, () => {
+      browser.get('');
 
-    let title = await JokePage.getTitleText();
+      const title = element(by.css('h1')).getText();
 
-    expect(title).toEqual('Chuck Norris Jokes');
+      expect(title).toEqual('Chuck Norris Jokes');
   });
 
-   it(`should have a new joke on button click`, async() => {
-    JokePage.navigateTo();
+  it(`should have different second joke`, async() => {
+    browser.get('');
 
-    let firstJoke = await JokePage.getParagraphText();
+    const joke1 = element(by.css('p')).getText();
+    const button = element(by.css('button')).click();
+    const joke2 = await element(by.css('p')).getText();
 
-    JokePage.getNextQuote();
+    expect(joke1).not.toEqual(joke2);
 
-    let secondJoke = await JokePage.getParagraphText();
-
-    expect(firstJoke).not.toEqual(secondJoke);
   });
 });
+
+// import { JokePage } from './joke.po';
+
+// describe(`Page: Joke Page`, () => {
+//   it(`should have a title of "Chuck Norris Jokes"`, async () => {
+//     JokePage.navigateTo();
+
+//     let title = await JokePage.getTitleText();
+
+//     expect(title).toEqual('Chuck Norris Jokes');
+//   });
+
+//    it(`should have a new joke on button click`, async() => {
+//     JokePage.navigateTo();
+
+//     let firstJoke = await JokePage.getParagraphText();
+
+//     JokePage.getNextQuote();
+
+//     let secondJoke = await JokePage.getParagraphText();
+
+//     expect(firstJoke).not.toEqual(secondJoke);
+//   });
+// });
